8bf8131927871f9ba9f289c2d00bf75ef3e93268
Vin'sBlog <91596049+VAB2007@users.noreply.github.com>
2022-01-28 10:18:05 +0530

    Add files via upload

---
+++ backend.php
@@ -0,0 +1,24 @@
+<?php 
+
+$con = mysqli_connect('localhost','root');
+
+if($con){
+	echo "Connected";
+}else{
+	echo "Disconnected";
+}
+
+mysqli_select_db($con, 'techchangedata');
+
+$user = $_POST['user'];
+$email = $_POST['email'];
+$mobile = $_POST['mobile'];
+$comment = $_POST['comment'];
+
+$query = "insert into userdata (user, email, mobile, comment) values ('$user', '$email', '$mobile', '$comment')";
+
+mysqli_query($con,$query);
+
+echo "$query";
+
+?>
